9d9c96141e9de3421107ce94ec56492a5cb1a6eb
Rayssa Freitas <rayssafreitas13@gmail.com>
2018-11-04 18:03:48 -0200

    5.6 - Prototipando listagem de endereços de clientes

--- PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/ClienteBean.java
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/ClienteBean.java
@@ -7,6 +7,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import com.algaworks.pedidovenda.model.Cliente;
+import com.algaworks.pedidovenda.model.Endereco;
 
 @ManagedBean
 @ViewScoped
@@ -19,6 +20,7 @@ public class ClienteBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Cliente cliente;
 	private List<Cliente> clientes;
+	private List<Endereco> enderecos;
 	
 	
 	public ClienteBean() {
@@ -27,9 +29,23 @@ public class ClienteBean implements Serializable {
 			clientes.add(criaCliente("Supermercado João das Couves Ltda", "Jurídica", "02.493.738/0001-83"));
 			clientes.add(criaCliente("Maria Conceição da Abadia", "Física", "02.493.738/0001-83"));
 			clientes.add(criaCliente("Supermercado Preço Bom Ltda", "Jurídica", "02.493.738/0001-83"));
+			
+			enderecos = new ArrayList<Endereco>();
+			enderecos.add(criaEndereco("Rua das Pedras Grandes Azuis", "1234", "Ap. 1022", "38499-533", "Uberlandia/MG"));
+			enderecos.add(criaEndereco("Av. Randon Pacheco", "455", "Sala 923", "38408-111", "Uberlandia/MG"));
 	
 	}
 	
+	private Endereco criaEndereco(String logradouro, String numero, String complemento, String cep, String cidadeUF) {
+		Endereco endereco = new Endereco();
+		endereco.setLogradouro(logradouro);
+		endereco.setNumero(numero);
+		endereco.setComplemento(complemento);
+		endereco.setCep(cep);
+		endereco.setCidadeUF(cidadeUF);
+		return endereco;
+	}
+
 	private Cliente criaCliente(String nome, String tipoPessoa, String cpf) {
 		Cliente cliente = new Cliente();
 		cliente.setNome(nome);
@@ -50,6 +66,16 @@ public class ClienteBean implements Serializable {
 	public void setClientes(List<Cliente> clientes) {
 		this.clientes = clientes;
 	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+	
+	
 	
 	
 	
