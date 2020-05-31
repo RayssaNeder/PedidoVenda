31caef3b6098086e9656c5324c5b16600f5aa3e8
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-05-31 15:10:47 -0300

    Desafio: criando entidades do cadastro de produtos

---
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/model/Categoria.java
@@ -0,0 +1,64 @@
+package com.algaworks.pedidovenda.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Categoria {
+	
+	@Id
+	private Long id;
+	private String descricao;
+	@OneToMany
+	private List<Categoria> subCategorias = new ArrayList<>();
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Categoria> getSubCategorias() {
+		return subCategorias;
+	}
+	public void setSubCategorias(List<Categoria> subCategorias) {
+		this.subCategorias = subCategorias;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
