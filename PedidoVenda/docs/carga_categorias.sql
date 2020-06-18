9b10b8f96e6adf24e266936cc2ab7349fc0505c9
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-06-18 01:10:58 -0300

    inciando arquivo que insere algumas categorias no banco

---
+++ PedidoVenda/docs/carga_categorias.sql
@@ -0,0 +1,6 @@
+insert into categoria (descricao) values('Computadores');
+insert into categoria (descricao) values('Notebooks');
+insert into categoria (descricao) values('Tablets');
+insert into categoria (descricao) values('Monitores');
+insert into categoria (descricao) values('Impressoras');
+insert into categoria (descricao) values('Acessórios');
