9e5efb49bc161c6725eb02d913199b6a9ee8c1de
Rayssa Neder <rayssafreitas13@gmail.com>
2020-07-19 18:03:20 -0300

    Implementando a pesquisa de pedidos

--- PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/PesquisaPedidosBean.java
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/PesquisaPedidosBean.java
@@ -1,36 +1,49 @@
 package com.algaworks.pedidovenda.controller;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import javax.faces.view.ViewScoped;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import com.algaworks.pedidovenda.model.Pedido;
+import com.algaworks.pedidovenda.model.StatusPedido;
+import com.algaworks.pedidovenda.repository.PedidosRepository;
+import com.algaworks.pedidovenda.repository.filter.PedidoFilter;
 
 @Named
 @ViewScoped
 public class PesquisaPedidosBean implements Serializable {
-	
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
-	private List<Integer> pedidosFiltrados;
+	
+	@Inject
+	private PedidosRepository pedidos;
+	
+	private PedidoFilter filtro;
+	private List<Pedido> pedidosFiltrados;
 	
 	public PesquisaPedidosBean() {
+		filtro = new PedidoFilter();
 		pedidosFiltrados = new ArrayList<>();
-		
-		for(int i = 0; i < 50; i++) {
-			pedidosFiltrados.add(i);
-		}
-		
 	}
 
-	public List<Integer> getPedidosFiltrados() {
+	public void pesquisar() {
+		pedidosFiltrados = pedidos.filtrados(filtro);
+	}
+	
+	public StatusPedido[] getStatuses() {
+		return StatusPedido.values();
+	}
+	
+	public List<Pedido> getPedidosFiltrados() {
 		return pedidosFiltrados;
 	}
-	
-	
 
+	public PedidoFilter getFiltro() {
+		return filtro;
+	}
+	
 }
